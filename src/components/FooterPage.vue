4b659ff0b50f11142d182adc3c1dfb1f932519e0
KingJosephh <aqaq6109@gmail.com>
2024-01-02 15:08:37 +0800

    init

---
+++ src/components/FooterPage.vue
@@ -0,0 +1,33 @@
+<template>
+    <section class="border-top border-info">
+        <div class="row d-flex justify-content-center pt-5 m-0 row-cols-lg-2 row-cols-1">
+            <div class="col-8">
+                <div class="row m-0">
+                    <div class="col d-flex justify-content-center mb-5">
+                        <i class="bi bi-person-hearts fs-s-104 text-info"></i>
+                        <div class="ps-4 d-flex flex-column justify-content-center">
+                            <h5>加入我們的團隊</h5>
+                            <P>我會賣車，希望能盡一份心力</P>
+                            <a href="./page4.html">
+                                <button class="btn btn-outline-dark"><p class="m-0 px-4 text-nowrap">聯絡我們</p></button>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col d-flex justify-content-center mb-5">
+                        <i class="bi bi-coin fs-s-104 text-info"></i>
+                        <div class="ps-4 d-flex flex-column justify-content-center">
+                            <h5>贊助我們的團隊</h5>
+                            <P>我有好車，請團隊估車</P>
+                            <a href="./page4.html">
+                                <button class="btn btn-outline-dark"><p class="m-0 px-4 text-nowrap">估車專欄</p></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer class="bg-dark text-white text-center py-3">
+        Copyright © 2021 芒果國際.All rights reserved.
+    </footer>
+</template>
--- src/views/UserBoard.vue
+++ src/views/UserBoard.vue
@@ -1,5 +1,185 @@
 <template>
     <div>
-        <h2>Hello</h2>
+      <NavBar></NavBar>
+      <section>
+        <div class="bg-img-size mb-5" style="background-image: url('@/assets/car-img/f-page3.jpg'); height: 652px;">
+            <!-- <div class="position-absolute text-center text-secondary" style="top: 50%; left: 15%; transform: translate(-25%, -50%); text-align: center;">  -->
+                <div class="position-absolute top-50 start-50 translate-middle">
+                <p class="display-1 fw-bold text-secondary">芒果 國際</p>
+                <hr class="mb-1 mx-n3 text-secondary">
+                <p class="h4 mt-3 text-center fw-bold text-info">英雄可以沒有家</p>
+                <p class="h4 mt-3 text-center fw-bold text-info">但是不能沒有戰馬</p>
+            </div>
+        </div>
+    </section>
+    <section class="mb-5">
+        <div class="row d-flex justify-content-center m-0">
+            <div class="col-lg-10 p-0">
+                <div class="row d-flex flex-row-reverse justify-content-center m-0 row-cols-1 row-cols-lg-2">
+                    <div class="col d-flex flex-column justify-content-center">
+                        <h2 class="kalma h4">Service</h2>
+                        <h2 class="fw-bold">您出錢 我出力 一起助浪浪</h2>
+                        <p>我們是一群車奴才，<br>
+                            希望能透過自己小小的力量幫助浪貓浪狗們， <br>
+                            可我們窮得只剩下超跑， <br>
+                            於是想到個兩全其美的好法子！<br>
+                            如果您購買我們的超跑，<br>
+                            我們將會把設計費80%全數捐給浪浪們！<br>
+                            您出錢，我出力，合作無間讓浪浪有飯吃，讚啦！</p>
+                    </div>
+                    <div class="col">
+                        <img class="garage-size" src="../assets/car-img/car-garage.jpg" alt="車庫圖">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container mb-5">
+            <div class="d-flex justify-content-center pb-5">
+                <img src="../assets/car-img/title1.png" alt="近期好車">
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="row g-4 m-0 row-cols-lg-4 row-cols-2 px-lg-0 px-4">
+                        <div class="col">
+                            <div class="card border-0 text-white show-card-detail">
+                                <img class="card-img car-model rounded-4" src="../assets/car-img/s63.jpg" alt="s63">
+                                <div class="card-img-overlay d-flex flex-column justify-content-end">
+                                    <p class="c2 fw-bold mb-0">車種</p>
+                                    <p class="c2 text-nowrap m-0 title">Mercedes-AMG s63</p>
+                                    <div class="c1 card-img-overlay gradient car-model rounded-4  ">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card border-0 text-white show-card-detail">
+                                <img class="card-img car-model rounded-4" src="../assets/car-img/911turbo.jpg" alt="911turbo">
+                                <div class="card-img-overlay d-flex flex-column justify-content-end">
+                                    <p class="c2 fw-bold mb-0">車種</p>
+                                    <p class="c2 text-nowrap m-0 title">porsche-911 turbo S</p>
+                                    <div class="c1 card-img-overlay gradient car-model rounded-4  ">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card border-0 text-white show-card-detail">
+                                <img class="card-img car-model rounded-4" src="../assets/car-img/x6.jpg" alt="x6">
+                                <div class="card-img-overlay d-flex flex-column justify-content-end">
+                                    <p class="c2 fw-bold mb-0">車種</p>
+                                    <p class="c2 text-nowrap m-0 title">BMW X6</p>
+                                    <div class="c1 card-img-overlay gradient car-model rounded-4  ">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card border-0 text-white show-card-detail">
+                                <img class="card-img car-model rounded-4" src="../assets/car-img/r4.jpg" alt="r4">
+                                <div class="card-img-overlay d-flex flex-column justify-content-end">
+                                    <p class="c2 fw-bold mb-0">車種</p>
+                                    <p class="c2 text-nowrap m-0 title">toyota Corolla Cross</p>
+                                    <div class="c1 card-img-overlay gradient car-model rounded-4  ">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="">
+        <div class="d-flex justify-content-center">
+            <img src="../assets/car-img/title2.png" alt="熱門車款">
+        </div>
+        <p class="text-center mb-6">這裡是在還在等家的浪浪們，若您正巧在找個家人，不訪來認識一下唄</p>
+        <div class="row px-lg-6 m-0 d-flex justify-content-center">
+            <div class="col-lg-12">
+                <div class="row m-0 row-cols-lg-3 row-cols-1 gy-3 px-4">
+                    <div class="col bg-transparent px-4">
+                        <div class="card border-0 hover-shadow-lg img-opacity rounded-4 h-100">
+                            <a href="./page6.html" class="card-link d-block h-100 link-unstyled text-reset text-decoration-none position-relative">
+                                <div class="card-header p-0">
+                                    <img class="rounded-top-4" src="../assets/car-img/mercedes.jpg" alt="mercedes logo">
+                                    <div class="car-bg-img" style="background-image: url('../assets/car-img/gt43.jpg');"></div>
+                                </div>
+                                <div class="card-body">
+                                    <div class="p-2">
+                                        <h5 class="fw-bold">Mercedes-GT43</h5>
+                                        <P class="mb-5">2012年款式，V8渦輪增壓Mercedes-AMG GT 4-Door Coupe擁有不容錯認的AMG GT家族化外觀，完美呼應強悍動力融合4座空間機能，四門跑車的空間完美隱藏在充滿張力的肌肉線條裡，從結合空氣力學的AMG直柵式水箱護罩與下方進氣壩即可辨識純正賽道血統。</P>
+                                    </div>
+                                </div>
+                                <div class="card-footer bg-white text-center border-0 rounded-4 position-absolute bottom-0 w-100">
+                                    <hr class="text-info mx-5">
+                                    <button class="kalma border-0 bg-transparent">MORE</button>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col bg-transparent px-4">
+                        <div class="card border-0 hover-shadow-lg img-opacity rounded-4 h-100">
+                            <a href="./page6.html" class="card-link d-block h-100 link-unstyled text-reset text-decoration-none position-relative">
+                                <div class="card-header p-0">
+                                    <img class="rounded-top-4" src="../assets/car-img/bmw.jpg" alt="bmw logo">
+                                    <div class="car-bg-img" style="background-image: url('@/assets/car-img/m4.jpg');"></div>
+                                </div>
+                                <div class="card-body">
+                                    <div class="p-2">
+                                        <h5 class="fw-bold">BMW-M4</h5>
+                                        <p class="pb-5">全新世代BMW M4強眼設計的無邊框雙腎型進氣格柵，營造強烈的視覺衝擊力，而簡潔的車身線條勾勒出靈動的輪廓，在不經意間，迸發強勢本色，併發更多對速度的渴望！</p>
+                                    </div>
+                                </div>
+                                <div class="card-footer bg-white text-center border-0 rounded-4 position-absolute bottom-0 w-100">
+                                    <hr class="text-info mx-5">
+                                    <button class="kalma border-0 bg-transparent">MORE</button>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col bg-transparent px-4">
+                        <div class="card border-0 hover-shadow-lg img-opacity rounded-4 h-100">
+                            <a href="./page6.html" class="card-link d-block h-100 link-unstyled text-reset text-decoration-none position-relative">
+                                <div class="card-header p-0">
+                                    <img class="rounded-top-4" src="../assets/car-img/porshe.jpg" alt="porsche logo">
+                                    <div class="car-bg-img" style="background-image: url('../assets/car-img/911turbo.jpg');"></div>
+                                </div>
+                                <div class="card-body">
+                                    <div class="p-2">
+                                        <h5 class="fw-bold">Porsche-911</h5>
+                                        <P class="mb-5">我們的工程師們非常喜歡將 911 Turbo 稱為最完美的跑車。具備極端的跑車性能卻同時非常舒適，且能夠無限制而完美地掌握日常生活所需。911 Turbo 系列持續貫徹自己的道路。
+                                        </P>
+                                    </div>
+                                </div>
+                                <div class="card-footer bg-white text-center border-0 rounded-4 position-absolute bottom-0 w-100">
+                                    <hr class="text-info mx-5">
+                                    <button class="kalma border-0 bg-transparent">MORE</button>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="d-flex justify-content-center mt-5 mb-6">
+        <a href="./page5.html">
+            <button class="btn btn-outline-dark rounded-pill"><p class="m-0 px-5">觀看更多坐駕</p></button>
+        </a>
+    </div>
+        <Footer></Footer>
     </div>
 </template>
+
+<script>
+import NavBar from '@/components/CarNavBar.vue'
+import Footer from '@/components/FooterPage.vue'
+export default {
+  components: {
+    NavBar,
+    Footer
+  }
+}
+</script>
